3a7a038260111c588eaa7527bce1049d53f8ad8e
bgora <bartlomiej.gora@infobip.com>
2022-03-03 14:21:35 +0100

    Google maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,13 @@
 import { User } from "./User";
 import { Company } from "./Company";
 console.log("hello");
+let user = new User();
+console.log(user);
+console.log(new Company());
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
